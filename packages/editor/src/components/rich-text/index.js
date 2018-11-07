8b9816ae127ce8a1b3d2b676d4cdf24bd5fe62ae
iseulde <wp@iseulde.com>
2018-11-07 21:30:57 +0100

    memoize createRecord

--- packages/editor/src/components/rich-text/index.js
+++ packages/editor/src/components/rich-text/index.js
@@ -99,6 +99,7 @@ export class RichText extends Component {
 		this.isActive = this.isActive.bind( this );
 
 		this.formatToValue = memize( this.formatToValue.bind( this ), { size: 1 } );
+		this.createRecordFromRange = memize( this.createRecordFromRange.bind( this ), { size: 1 } );
 
 		this.savedContent = value;
 		this.patterns = getPatterns( {
@@ -227,8 +228,10 @@ export class RichText extends Component {
 	}
 
 	createRecord() {
-		const range = getSelection().getRangeAt( 0 );
+		return this.createRecordFromRange( getSelection().getRangeAt( 0 ) );
+	}
 
+	createRecordFromRange( range ) {
 		return create( {
 			element: this.editableRef,
 			range,
